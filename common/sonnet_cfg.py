33a5b39664eae706862569167f5c5894868277f4
logan472 <157203024+logan472@users.noreply.github.com>
2024-03-28 23:59:50 +1100

    Update sonnet_cfg.py

--- common/sonnet_cfg.py
+++ common/sonnet_cfg.py
@@ -1,7 +1,7 @@
 # Configuration File
 
 # Default global prefix, can be set per server
-GLOBAL_PREFIX = '!'
+GLOBAL_PREFIX = '?'
 # Default action to occur when someone breaks blacklist, can be set per server
 BLACKLIST_ACTION = "warn"
 # Default emoji to use for starboard, can be set per server
@@ -10,12 +10,14 @@ STARBOARD_EMOJI = "⭐"
 STARBOARD_COUNT = "5"
 
 # chose between using mariadb or sqlite3
-DB_TYPE = "mariadb"
+DB_TYPE = "sqlite3"
 # only needs to be set if using sqlite3 db in sonnet mode, mariadb login is stored in .login-info.txt
 SQLITE3_LOCATION = "datastore/sonnetdb.db"
 
 # Configure whether to use re2 or re, any public instance must use re2 due to exploits, however re is cross platform and easier to set up
-REGEX_VERSION = "re2"
+REGEX_VERSION = "re"
 
 # Configure whether or not to load the C loader
 CLIB_LOAD = True
+
+BOT_NAME = "Verde"
